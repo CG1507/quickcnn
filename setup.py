04d0be994c4e70ccb29e40774badafd88aae3592
CG1507 <g8ghanshym@gmail.com>
2018-12-15 19:38:59 +0530

    :tada: setup.py updated

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-	long_description = fh.read()
+long_description="Main idea of QuickCNN is to train deep ConvNet without diving into architectural details. QuickCNN works as an interactive tool for transfer learning, finetuning, and scratch training with custom datasets. It has pretrained model zoo and also works with your custom keras model architecture."
 
 setuptools.setup(
 	name="quickcnn",
-	version="0.0.5",
+	version="0.0.10",
 	author="Ghanshyam Chodavadiya",
 	author_email="g8ghanshym@gmail.com",
-	description="Library for ConvNet training on colab",
+	description="ConvNet architectures on Google-Colaboratory",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/CG1507/quickcnn",
